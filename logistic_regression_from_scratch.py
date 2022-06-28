9ce72074696a1ac3868ba6fa3430b3e9fcb1b17f
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2022-06-28 07:12:15 +0000

    Modify

--- logistic_regression_from_scratch.py
+++ logistic_regression_from_scratch.py
@@ -16,6 +16,14 @@ def logistic_regression(features, target, num_steps, lr, add_intercept=False):
     if add_intercept: features = np.hstack((np.ones((features.shape[0], 1)), features))
     weights = np.zeros(features.shape[1])
     for step in range(num_steps):
-        scores = features.dot(weights)
-        predictions = sigmoid(scores)
-        
+        weights += lr * features.T.dot(target - sigmoid(features.dot(weights)))
+        if step % 10000 == 0: print(log_likelihodd(features, target, weights))
+    return weights
+
+weights = logistic_regression(simulated_separable_features, simulated_labels, 50000, 5e-5, True)
+print('LOGISTIC REGRESSION FROM SCRATCH WEIGHTS => ', weights)
+preds = np.round(sigmoid(np.hstack((np.ones((simulated_separable_features.shape[0], 1)), simulated_separable_features)).dot(weights)))
+print(f'Accuracy from scratch: {(preds == simulated_labels).sum().astype(float) / len(preds)}')
+plt.figure(figsize=(12, 8))
+plt.scatter(simulated_separable_features[:, 0], simulated_separable_features[:, 1], c=preds == simulated_labels - 1, alpha=0.8, s=50)
+plt.show()
